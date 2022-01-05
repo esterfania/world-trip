3804376a4f832b73dde7b7c0111241e21258762a
Ester Gama <esterfaniasantos@gmail.com>
2022-01-05 10:38:43 -0300

    chore: install react testing library cypress

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -2,7 +2,15 @@
 
 describe('Cypress TS',() => {
 
-    it('should go to Google', () => {
+    it.skip('should go to Google', () => {
         cy.google();
+    })
+
+    it('should change light/dark theme on willian justen site', ()=> {
+        cy.visit('https://willianjusten.com.br/');
+        cy.findByTitle(/Mudar o Tema/i).click(); 
+        cy.get('.light').should('exist');
+        cy.findByTitle(/Mudar o Tema/i).click(); 
+        cy.get('.dark').should('exist');
     })
 })
